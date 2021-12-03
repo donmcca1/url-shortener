6450921a6e707606b44951fc5d48001457860789
ezmccdo <don.mccarthy@ericsson.com>
2021-12-03 11:21:29 +0000

    remove copyright headers

--- src/com/enel/x/url/service/UrlServiceImpl.java
+++ src/com/enel/x/url/service/UrlServiceImpl.java
@@ -1,19 +1,6 @@
-/*------------------------------------------------------------------------------
- *******************************************************************************
- * COPYRIGHT Ericsson 2021
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- *******************************************************************************
- *----------------------------------------------------------------------------*/
-
 package com.enel.x.url.service;
 
 import java.net.URI;
-import java.util.Optional;
 
 import com.enel.x.url.repository.UrlRepo;
 import com.enel.x.url.service.domain.UriProcessor;
--- src/com/enel/x/url/service/domain/UriValidator.java
+++ src/com/enel/x/url/service/domain/UriValidator.java
@@ -1,15 +1,3 @@
-/*------------------------------------------------------------------------------
- *******************************************************************************
- * COPYRIGHT Ericsson 2021
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- *******************************************************************************
- *----------------------------------------------------------------------------*/
-
 package com.enel.x.url.service.domain;
 
 import java.net.URI;
